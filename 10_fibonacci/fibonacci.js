8cfa7ec888589a8051c073d39761b919611dca61
Wong Wei Jian <weijian9091@gmail.com>
2022-11-10 16:01:34 +0800

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 1) {
+        return 1;
+    } else if (num == 0) {
+        return 0;
+    } else if (num < 0) {
+        return 'OOPS'
+    } else {
+        return fibonacci(num-1) + fibonacci(num-2);
+    }
 };
+
+// 0 + 1 (2nd term)
+// 1 + 1 (3rd term)
+// 2 + 1 (4th term)
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
