554e7929e1bdff87597539d00a1860ca3b288702
Wong Wei Jian <weijian9091@gmail.com>
2022-11-10 15:34:54 +0800

    completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // use regex to replace special characters with empty str
+    const newStr = str.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    const arr = newStr.split('');
+    const backwardsArr = [];
+    for (let i = arr.length -1; i >= 0 ; i-- ) {
+        backwardsArr.push(arr[i]);
+    }
+    if (arr.toString() === backwardsArr.toString()){
+        return true;
+    } else {return false};
 };
 
 // Do not edit below this line
