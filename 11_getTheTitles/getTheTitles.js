f1ca1fc2ff935b0d0a23d85c68ce45c88cbdee7f
Wong Wei Jian <weijian9091@gmail.com>
2022-11-10 16:06:11 +0800

    complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    const newArr = arr.map((item)=>{
+        return item['title'];
+    });
+    return newArr;
 };
 
 // Do not edit below this line
