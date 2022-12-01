6c3fc758fe36e1589a528d78dd394c1d71b9a44f
Wong Wei Jian <weijian9091@gmail.com>
2022-12-02 02:32:38 +0800

    Finish the exercise findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let Aage;
+    let Bage;
+    const currentYear = new Date().getFullYear();
+    arr.sort((a,b)=> {
+        if (a.yearOfDeath === undefined){
+             Aage = currentYear - a.yearOfBirth;
+             Bage = b.yearOfDeath - b.yearOfBirth;
+        } else if (b.yearOfDeath === undefined){
+            Bage = currentYear - b.yearOfBirth;
+            Aage = a.yearOfDeath - a.yearOfBirth;
+        } else {
+            Aage = a.yearOfDeath - a.yearOfBirth;
+            Bage = b.yearOfDeath - b.yearOfBirth;
+        };
 
+        if (Aage > Bage){
+            return -1;
+        } else return 1;
+    });
+    return arr[0];
 };
 
 // Do not edit below this line
